9965e02567e36350f4aaa0a34d2039c394c6056e
Hobbes4 <kylefertile@gmail.com>
2015-11-16 21:46:39 -0800

    Initial commit

---
+++ app/src/main/java/com/example/beequiet/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.beequiet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Bee Quiet");
+    }
+
+    public void clickView(View v){
+        Intent intent = new Intent(this, ViewActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickHour(View v){
+        // Silent for an hour, set the text of the button to current amount of time left
+    }
+
+    public void clickDeactivate(View v){
+        // Stop the effects of the app or vice versa
+
+    }
+
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+}
---
+++ app/src/main/java/com/example/beequiet/ViewActivity.java
@@ -0,0 +1,29 @@
+package com.example.beequiet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Bee Quiet");
+
+
+    }
+
+    public void clickImport(View v){
+        Intent intent = new Intent(this, ImportActivity.class);
+        startActivity(intent);
+    }
+
+}
