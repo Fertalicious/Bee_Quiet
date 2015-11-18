9b9f3fafe9dfdc2badd7810ab01efc1f098a9a81
RustyNail-27 <russell.d.g.walton@gmail.com>
2015-11-17 16:25:00 -0800

    test

--- app/src/main/java/com/example/beequiet/MainActivity.java
+++ app/src/main/java/com/example/beequiet/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Bee Quiet");
+        //test russ
     }
 
     public void clickView(View v){
