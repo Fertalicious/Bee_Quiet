c2db6c8f39688caedcdb6c70c3f4e149a97aabbd
RustyNail-27 <russell.d.g.walton@gmail.com>
2015-11-22 22:25:54 -0800

    Russ nov22 10:25

--- app/src/main/java/com/example/beequiet/MainActivity.java
+++ app/src/main/java/com/example/beequiet/MainActivity.java
@@ -12,6 +12,29 @@ import android.content.Context;
 import android.content.BroadcastReceiver;
 import android.content.IntentFilter;
 import android.widget.ToggleButton;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Handler.Callback;
+import android.widget.TextView;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import java.util.Calendar;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import java.io.File;
+import java.lang.NumberFormatException;
+import android.media.AudioManager;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
     int timeLeft;
     MyReceiver myReceiver;
     ToggleButton onOff;
+    private AudioManager myAudioManager;
+
+
+
     private class MyReceiver extends BroadcastReceiver{
 
         @Override
@@ -115,6 +142,138 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
     }
 
+
+    /*
+    public class AlarmReciever extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+
+            // here you can start an activity or service depending on your need
+
+        }
+
+    }
+*/
+/*
+    public class checkSilent extends IntentService {
+        private static final String TAG = "MyService";
+
+        // Service no-arg constructor
+        public checkSilent() {
+            super(TAG);
+        }
+
+        @Override
+        public void onHandleIntent(Intent intent) {
+        // runs checkSchedule function to determine if current time is within silent time ranges
+            if(checkSchedule())
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            else
+                myAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        }
+    }
+*/
+    public boolean checkSchedule() {
+        boolean silentTime = false;
+        String daySchedule;
+        String weekDay = "";
+        String timeStart = "";
+        String timeEnd = "";
+        String currentDay = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(System.currentTimeMillis());
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        int currentMinute = Calendar.getInstance().get(Calendar.MINUTE);
+        String[] timeStartArray;
+        String[] timeEndArray;
+        int startHour = 0;
+        int startMinute = 0;
+        int endHour = 0;
+        int endMinute= 0;
+
+        FileInputStream fIn = null;
+        try {
+            fIn = openFileInput("time.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fIn));
+
+
+        try{
+
+
+
+            while ((daySchedule = reader.readLine()) != null) {
+                // process the current line, splitting into array of Strings
+                String[] str_array = daySchedule.split("-");
+
+                for(int i = 0; i < str_array.length; i++)
+                {
+                    if(i == 0) {
+                        //checks if first String in array is equal to current system day, breaks out if not
+                        weekDay = str_array[i];
+                        if(!weekDay.equals(currentDay))
+                            break;
+                    }
+                    else if(i%2 == 1) {
+                        timeStart = str_array[i];
+                        timeStartArray = timeStart.split(":");
+                        try {
+                            startHour = Integer.parseInt(timeStartArray[0]);
+                        } catch(NumberFormatException nfe) {}
+                        try {
+                            startMinute = Integer.parseInt(timeStartArray[1]);
+                        } catch(NumberFormatException nfe) {}
+
+                    }
+                    else {
+                        timeEnd = str_array[i];
+                        timeEndArray = timeEnd.split(":");
+                        try {
+                            endHour = Integer.parseInt(timeEndArray[0]);
+                        } catch(NumberFormatException nfe) {}
+                        try {
+                            endMinute = Integer.parseInt(timeEndArray[1]);
+                        } catch(NumberFormatException nfe) {}
+
+                        //at this point there is a timeStart and timeEnd, then compares system time to the range
+                        if(currentHour >= startHour && currentHour < endHour)
+                            silentTime = true;
+                    }
+
+                }
+
+                if(weekDay.equals(currentDay))
+                    break;
+            }
+
+        }
+        catch (Exception e){
+        }
+
+
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //returns false if the current system time doesn't fall into any start or end ranges for current day
+        return silentTime;
+    }
+
+    public void checkFileExist(){
+        try{ FileInputStream fis = null;
+            fis = openFileInput("time.txt");
+            // File file = new File("time.txt");
+            // boolean exists = file.isFile();
+        }
+        catch (Exception e){
+
+        }
+    }
 
 /*
     @Override
